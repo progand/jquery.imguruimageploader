2d42e1de3439302ef228040ef19f9fe87692e7c2
progand <lach@ukr.net>
2015-03-11 11:03:26 +0200

    [*] AMD/CommonJS support

--- public_html/js/jquery.imguruimageploader.js
+++ public_html/js/jquery.imguruimageploader.js
@@ -1,5 +1,22 @@
-;
-(function ($, window) {
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        // Register as an anonymous AMD module:
+        define([
+            'jquery',
+            'jquery.ui.widget'
+        ], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS:
+        factory(
+            require('jquery'),
+            require('./vendor/jquery.ui.widget')
+        );
+    } else {
+        // Browser globals:
+        factory(window.jQuery);
+    }
+}(function ($) {
     $.widget("livarava.imgurimageuploader", {
         // Options to be used as defaults
 
@@ -66,4 +83,4 @@
             this._super("_setOption", key, value);
         }
     });
-})(jQuery, window);
+}));
